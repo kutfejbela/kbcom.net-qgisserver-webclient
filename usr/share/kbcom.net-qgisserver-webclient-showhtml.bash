7478e019264d5aa8bfdc891ed7e2adcc9ca6167e
root <root@szerver05.petav.local>
2018-09-19 15:47:32 +0200

    HTML comments

--- usr/share/kbcom.net-qgisserver-webclient-showhtml.bash
+++ usr/share/kbcom.net-qgisserver-webclient-showhtml.bash
@@ -4,23 +4,21 @@ echo "<table style='margin: auto; border: 0;'>"
 echo "<tr style='border: 0;'>"
 echo "<td style='border: 0;'>"
 
-echo
-echo "<!-- HTML template processing has started -->"
-echo
+printf '\n<!-- HTML template processing has started (%(%c)T) -->\n\n'
 
 for (( GLOBAL_HTML_INDEX=0; GLOBAL_HTML_INDEX<=CONFIG_HTML_COUNT; GLOBAL_HTML_INDEX++ ))
 do
-  echo
-  echo "<!-- HTML template index: $GLOBAL_HTML_INDEX -->"
-  echo
+ printf '\n<!-- HTML template starting, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
 
  if [ "${CONFIG_HTML_ISSTATIC[$GLOBAL_HTML_INDEX]}" = true ]
  then
   echo -e "${CONFIG_HTML_TEMPLATE[$GLOBAL_HTML_INDEX,html_body]}"
  else
+  printf '\n<!-- HTML template quering, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
   IFS=$'\n'
   GLOBAL_HTML_DATA=$(eval "${CONFIG_HTML_EXECUTE[$GLOBAL_HTML_INDEX]}")
 
+  printf '\n<!-- HTML template showing, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
   if [ -z "$GLOBAL_HTML_DATA" ]
   then
    echo -e "${CONFIG_HTML_EMPTY[$GLOBAL_HTML_INDEX]}"
@@ -44,9 +42,7 @@ do
  fi
 done
 
-echo
-echo "<!-- HTML template processing finished -->"
-echo
+printf '\n<!-- HTML template processing has finished (%(%c)T) -->\n\n'
 
 echo "</td>"
 echo "</tr>"
