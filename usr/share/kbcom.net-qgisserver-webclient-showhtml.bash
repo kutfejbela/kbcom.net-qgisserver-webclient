a091409695c585abf036acea5ad3b114269e2e81
root <root@szerver05.petav.local>
2018-09-20 15:06:10 +0200

    HTML comments

--- usr/share/kbcom.net-qgisserver-webclient-showhtml.bash
+++ usr/share/kbcom.net-qgisserver-webclient-showhtml.bash
@@ -8,27 +8,34 @@ printf '\n<!-- HTML template processing has started (%(%c)T) -->\n\n'
 
 for (( GLOBAL_HTML_INDEX=0; GLOBAL_HTML_INDEX<=CONFIG_HTML_COUNT; GLOBAL_HTML_INDEX++ ))
 do
- printf '\n<!-- HTML template starting, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
+ printf '\n<!-- ### HTML template starting, index: %s (%(%c)T) ### -->\n\n' "$GLOBAL_HTML_INDEX"
 
  if [ "${CONFIG_HTML_ISSTATIC[$GLOBAL_HTML_INDEX]}" = true ]
  then
+  printf '\n<!-- HTML template: static, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
   echo -e "${CONFIG_HTML_TEMPLATE[$GLOBAL_HTML_INDEX,html_body]}"
  else
   printf '\n<!-- HTML template quering, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
   IFS=$'\n'
   GLOBAL_HTML_DATA=$(eval "${CONFIG_HTML_EXECUTE[$GLOBAL_HTML_INDEX]}")
 
-  printf '\n<!-- HTML template showing, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
   if [ -z "$GLOBAL_HTML_DATA" ]
   then
+   printf '\n<!-- HTML template: empty data, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
    echo -e "${CONFIG_HTML_EMPTY[$GLOBAL_HTML_INDEX]}"
   else
+   printf '\n<!-- HTML template: data header, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
    echo -e "${CONFIG_HTML_TEMPLATE[$GLOBAL_HTML_INDEX,html_header]}"
 
    if [ ! -z "${CONFIG_HTML_TEMPLATE[$GLOBAL_HTML_INDEX,html_body]}" ]
    then
+    GLOBAL_DATA_INDEX=0
+
     for GLOBAL_STRINGDATAROW in $GLOBAL_HTML_DATA
     do
+     ((GLOBAL_DATA_INDEX++))
+     printf '\n<!-- HTML template: data row, index: %s, %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX" "$GLOBAL_DATA_INDEX"
+
      declare -a GLOBAL_STRINGDATAROW_ARRAY
      convert_arraydatarow_stringdatarow "$GLOBAL_STRINGDATAROW"
 
@@ -37,6 +44,7 @@ do
     done
    fi
 
+   printf '\n<!-- HTML template: data footer, index: %s (%(%c)T) -->\n\n' "$GLOBAL_HTML_INDEX"
    echo -e "${CONFIG_HTML_TEMPLATE[$GLOBAL_HTML_INDEX,html_footer]}"
   fi
  fi
